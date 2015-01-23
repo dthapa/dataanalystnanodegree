ddce01a99446ac4a7bd52b3cbd8e892dc13d00f8
Don <donaththapa@gmail.com>
2015-01-23 05:25:30 -0600

    used for the short answers for rain/no-rain histogram

---
+++ project_4/exercise_3_histogram/data_visualization.py
@@ -0,0 +1,50 @@
+from pandas import *
+from ggplot import *
+import pandasql as pds
+
+def plot_weather_data(turnstile_weather):
+    ''' 
+    Use ggplot to make another data visualization focused on the MTA and weather
+    data we used in assignment #3. You should make a type of visualization different
+    than you did in exercise #1, and try to use the data in a different way (e.g., if you
+    made a lineplot concerning ridership and time of day in exercise #1, maybe look at weather
+    and try to make a histogram in exercise #2). 
+    
+    You should feel free to implement something that we discussed in class
+    (e.g., scatterplots, line plots, or histograms) or attempt to implement 
+    something more advanced if you'd like.  Here are some suggestions for things
+    to investigate and illustrate:
+    * Ridership by time of day or day of week
+    * How ridership varies based on Subway station
+    * Which stations have more exits or entries at different times of day
+
+    If you'd like to learn more about ggplot and its capabilities, take
+    a look at the documentation at:
+    https://pypi.python.org/pypi/ggplot/
+    
+    You can check out: 
+    https://www.dropbox.com/s/meyki2wl9xfa7yk/turnstile_data_master_with_weather.csv
+     
+    To see all the columns and data points included in the turnstile_weather 
+    dataframe. 
+    
+    However, due to the limitation of our Amazon EC2 server, we are giving you about 1/3
+    of the actual data in the turnstile_weather dataframe
+    '''
+
+    df = turnstile_weather
+    plot = ggplot(df, aes(x='ENTRIESn_hourly', color='rain')) + \
+            geom_bar(binwidth = 500) + scale_x_continuous(limits=[0, 10000]) + \
+            ggtitle('Entries per hour by rain conditions') + \
+            xlab('Entries range (bin=500)') + ylab('Entries per hour')
+    
+    print plot
+    return plot
+
+if __name__ == "__main__":
+    image = "plot.png"
+    with open(image, "wb") as f:
+        turnstile_weather = pd.read_csv(input_filename)
+        turnstile_weather['datetime'] = turnstile_weather['DATEn'] + ' ' + turnstile_weather['TIMEn']
+        gg =  plot_weather_data(turnstile_weather)
+        #ggsave(f, gg)
